341dace22d9e762b470ce5bd17f3a327f2fd075b
tlavksdmlskf <127850569+tlavksdmlskf@users.noreply.github.com>
2024-10-16 17:35:27 +0900

    Add files via upload

---
+++ review/remoteContral/RemoteControl.java
@@ -0,0 +1,9 @@
+package review.remoteContral;
+
+public interface RemoteControl {
+    void setPower(boolean power); // 전원 온오프
+    void setChannel(int channel); // 특정 채널로 이동
+    void nextChannel(); // 다음 채널로 이동
+    void previousChannel(); // 이전 채널로 이동
+    void adjustVolume(int level);
+}
